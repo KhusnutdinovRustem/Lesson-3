74acb8556eed7405479107e230ed5eca38a01c49
KhusnutdinovRustem <167807173+KhusnutdinovRustem@users.noreply.github.com>
2024-05-01 23:45:29 +0300

    Add files via upload

--- Homework2.py
+++ Homework2.py
@@ -2,7 +2,7 @@ name = "Rustem"
 print(name)
 age = 33
 print(age)
-new_age = age+1
-print(new_age)
-is_student = 'true'
-print('is_student:' + is_student)
+print('new_age:', age+1)
+is_student = True
+print(type(is_student))
+print('is_student:', is_student)
